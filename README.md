bc1b89b860e8a22ac6e30f257992a232bc53119b
Amo Moloko <u14284783@tuks.co.za>
2018-08-15 14:31:03 +0200

    configuring environments

--- README.md
+++ README.md
@@ -47,10 +47,8 @@ And run.
 $ npm run deploy
 ```
 
-#### Maintainers
-
-Serverless Stack is authored and maintained by Frank Wang ([@fanjiewang](https://twitter.com/fanjiewang)) & Jay V ([@jayair](https://twitter.com/jayair)). [**Subscribe to our newsletter**](http://eepurl.com/cEaBlf) for updates on Serverless Stack. Send us an [email][Email] if you have any questions.
-
-[Email]: mailto:contact@anoma.ly
+``` 
+REACT_APP_STAGE=prod npm run build
+```
 
 
--- src/config.js
+++ src/config.js
@@ -1,17 +1,45 @@
-export default {
-  MAX_ATTACHMENT_SIZE: 5000000,
+const dev = {
   s3: {
-    REGION: "us-east-1",
-    BUCKET: "notes-app-uploads"
+    REGION: "us-east-1", 
+    BUCKET: "notes-app-2-api-dev-attachmentsbucket-7q5rew6kvuw7"
   },
   apiGateway: {
     REGION: "us-east-1",
-    URL: "https://5by75p4gn3.execute-api.us-east-1.amazonaws.com/prod"
+    URL: "https://h7n17rwick.execute-api.us-east-1.amazonaws.com/dev"
   },
   cognito: {
     REGION: "us-east-1",
-    USER_POOL_ID: "us-east-1_udmFFSb92",
-    APP_CLIENT_ID: "4hmari2sqvskrup67crkqa4rmo",
-    IDENTITY_POOL_ID: "us-east-1:ceef8ccc-0a19-4616-9067-854dc69c2d82"
+    USER_POOL_ID: "us-east-1_xpSvl7StT",
+    APP_CLIENT_ID: "2uq6cl8rltlqfn918ekvfh53cr",
+    IDENTITY_POOL_ID: "us-east-1:9348af54-1d62-482b-b0da-32062d3d4b5a"
   }
+};
+
+const prod = {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "notes-app-2-api-prod-serverlessdeploymentbucket-oprm2x3t3vrv"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://816ofmqs9b.execute-api.us-east-1.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_dY65GfroE",
+    APP_CLIENT_ID: "125g15nn4f453mh8ck3aj1tpc0",
+    IDENTITY_POOL_ID: "us-east-1:ae6c2dc1-9b42-45d3-8b87-f166f93a33cd"
+  }
+};
+
+
+//default to dev if not set 
+const config = process.env.REACT_APP_STAGE === 'prod'
+  ? prod
+  : dev;
+
+export default {
+  //Add common config values here
+  MAX_ATTACHMENT_SIZE: 5000000,
+  ...config
 };
